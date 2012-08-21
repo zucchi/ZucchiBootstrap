daf89d8d31f2c87995eaf3436609d5354033b1ec
Matt Cockayne <matt@zucchi.co.uk>
2012-08-21 23:56:09 +0100

    updated bootstrapmessenger

--- src/ZucchiBootstrap/Controller/Plugin/BootstrapMessenger.php
+++ src/ZucchiBootstrap/Controller/Plugin/BootstrapMessenger.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * ZucchiBootstrap (http://zucchi.co.uk)
  *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @link      http://github.com/zucchi/ZucchiBootstrap for the canonical source repository
+ * @copyright Copyright (c) 2012 Zucchi Limited (http://zucchi.co.uk)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Mvc
  */
 
 namespace ZucchiBootstrap\Controller\Plugin;
@@ -21,11 +20,12 @@ use Zend\Session\SessionManager;
 use Zend\Stdlib\SplQueue;
 
 /**
- * Flash Messenger - implement session-based messages
- *
- * @category   Zend
- * @package    Zend_Mvc
- * @subpackage Controller\Plugin
+ * Clone of FlashMessenger plugin adapted to work with Bootstrap
+ * 
+ * @author Matt Cockayne <matt@zucchi.co.uk>
+ * @package ZucchiBootstrap
+ * @subpackage Controller
+ * @category Plugin
  */
 class BootstrapMessenger extends AbstractPlugin implements IteratorAggregate, Countable
 {
